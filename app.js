993516d56f9bd77158d1d5f5d63fda89609d9a0c
khaledghaffarii <med.khaledghaffary@gmail.com>
2022-06-01 10:22:33 +0100

    build a order featcher

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.options('*', cors())
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-
-
+app.use(authJwt());
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
@@ -46,5 +46,6 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 
 //Server
 app.listen(3000, ()=>{
-     console.log('server is running http://localhost:3000');
+
+    console.log('server is running http://localhost:3000');
 })
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,16 +1,16 @@
-const expressJwt = require('express-jwt');
+const { expressjwt: jwt } = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-    return expressJwt({
+    return jwt({
         secret,
         algorithms: ['HS256'],
         isRevoked: isRevoked
     }).unless({
         path: [
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
         ]
@@ -18,7 +18,7 @@ function authJwt() {
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
+    if (!payload.isAdmin) {
         done(null, true)
     }
 
